d022e11d406d7d9a96c1a489e1a9cfb7b0d23a02
Marin Rados <radosmarin35@gmail.com>
2024-03-13 11:15:22 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Layout from "./components/layout";
+import "./styles/styles.scss";
+
+const App = () => {
+  return (
+    <>
+      <Layout>hello</Layout>
+    </>
+  );
+};
+
+export default App;
